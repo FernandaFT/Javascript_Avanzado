11117c4ac40fb88f632d23a229909d7652b77df2
Fernanda Fajardo <nanda199784@gmail.com>
2017-08-15 11:25:16 -0600

    'ThirdHomework_GalleryThreeJS'

---
+++ Gallery_ThreeJS/html/js/main.js
@@ -0,0 +1,246 @@
+var table = [
+	[ "../img/image1.jpg", 1, 1 ],
+	[ "../img/image1.jpg", 18, 1 ],
+	[ "../img/image1.jpg", 1, 2 ],
+	[ "../img/image1.jpg", 2, 2 ],
+	[ "../img/image1.jpg", 13, 2 ],
+	[ "../img/image1.jpg", 14, 2 ],
+	[ "../img/image1.jpg", 15, 2 ],
+	[ "../imgimage1.jpg", 16, 2 ],
+	[ "../img/image1.jpg", 17, 2 ],
+	[ "../img/image1.jpg", 18, 2 ],
+	[ "../img/image1.jpg", 1, 3 ],
+	[ "../img/image1.jpg", 2, 3 ],
+	[ "../img/image1.jpg", 13, 3 ],
+	[ "../img/image1.jpg", 14, 3 ],
+	[ "../img/image1.jpg", 15, 3 ],
+	[ "../img/image1.jpg", 16, 3 ],
+	[ "../img/image1.jpg", 17, 3 ],
+	[ "../img/image1.jpg", 18, 3 ],
+	[ "../img/image1.jpg", 1, 4 ],
+	[ "../img/image1.jpg", 2, 4 ],
+	[ "../img/image1.jpg", 3, 4 ],
+	[ "../img/image1.jpg", 4, 4 ],
+	[ "../img/image1.jpg", 5, 4 ],
+	[ "../img/image1.jpg", 6, 4 ],
+	[ "../img/image1.jpg", 7, 4 ],
+	[ "../img/image1.jpg", 8, 4 ],
+	[ "../img/image1.jpg", 9, 4 ],
+	[ "../img/image1.jpg", 10, 4 ],
+	[ "../img/image1.jpg", 11, 4 ],
+	[ "../img/image1.jpg", 12, 4 ],
+	[ "../img/image1.jpg", 13, 4 ],
+	[ "../img/image1.jpg", 14, 4 ],
+	[ "../img/image1.jpg", 15, 4 ],
+	[ "../img/image1.jpg", 16, 4 ],
+	[ "../img/image1.jpg", 17, 4 ],
+	[ "../img/image1.jpg", 18, 4 ],
+	[ "../img/image1.jpg", 1, 5 ],
+	[ "../img/image1.jpg", 2, 5 ],
+	[ "../img/image1.jpg", 3, 5 ],
+	[ "../img/image1.jpg", 4, 5 ],
+	[ "../img/image1.jpg", 5, 5 ],
+	[ "../img/image1.jpg", 6, 5 ],
+	[ "../img/image1.jpg", 7, 5 ],
+	[ "../img/image1.jpg", 8, 5 ],
+	[ "../img/image1.jpg", 9, 5 ],
+	[ "../img/image1.jpg", 10, 5 ],
+	[ "../img/image1.jpg", 11, 5 ],
+	[ "../img/image1.jpg", 12, 5 ],
+	[ "../img/image1.jpg", 13, 5 ],
+	[ "../img/image1.jpg", 14, 5 ],
+	[ "../img/image1.jpg", 15, 5 ],
+	[ "../img/image1.jpg", 16, 5 ],
+	[ "../img/image1.jpg", 17, 5 ],
+	[ "../img/image1.jpg", 18, 5 ],
+	[ "../img/image1.jpg", 1, 6 ],
+	[ "../img/image1.jpg", 2, 6 ],
+	[ "../img/image1.jpg", 4, 9 ],
+	[ "../img/image1.jpg", 5, 9 ],
+	[ "../img/image1.jpg", 6, 9 ],
+	[ "../img/image1.jpg", 7, 9 ],
+	[ "../img/image1.jpg", 8, 9 ],
+	[ "../img/image1.jpg", 9, 9 ],
+	[ "../img/image1.jpg", 10, 9 ],
+	[ "../img/image1.jpg", 11, 9 ],
+	[ "../img/image1.jpg", 12, 9 ],
+	[ "../img/image1.jpg", 13, 9 ],
+	[ "../img/image1.jpg", 14, 9 ],
+	[ "../img/image1.jpg", 15, 9 ],
+	[ "../img/image1.jpg", 16, 9 ],
+	[ "../img/image1.jpg", 17, 9 ],
+	[ "../img/image1.jpg", 18, 9 ],
+	[ "../img/image1.jpg", 4, 6 ],
+	[ "../img/image1.jpg", 5, 6 ],
+	[ "../img/image1.jpg", 6, 6 ],
+	[ "../img/image1.jpg", 7, 6 ],
+	[ "../img/image1.jpg", 8, 6 ],
+	[ "../img/image1.jpg", 9, 6 ],
+	[ "../img/image1.jpg", 10, 6 ],
+	[ "../img/image1.jpg", 11, 6 ],
+	[ "../img/image1.jpg", 12, 6 ],
+	[ "../img/image1.jpg", 13, 6 ],
+	[ "../img/image1.jpg", 14, 6 ],
+	[ "../img/image1.jpg", 15, 6 ],
+	[ "../img/image1.jpg", 16, 6 ],
+	[ "../img/image1.jpg", 17, 6 ],
+	[ "../img/image1.jpg", 18, 6 ],
+	[ "../img/image1.jpg", 1, 7 ],
+	[ "../img/image1.jpg", 2, 7 ],
+	[ "../img/image1.jpg", 4, 10 ],
+	[ "../img/image1.jpg", 5, 10 ],
+	[ "../img/image1.jpg", 6, 10 ],
+	[ "../img/image1.jpg", 7, 10 ],
+	[ "../img/image1.jpg", 8, 10 ],
+	[ "../img/image1.jpg", 9, 10 ],
+	[ "../img/image1.jpg", 10, 10 ],
+	[ "../img/image1.jpg", 11, 10 ],
+	[ "../img/image1.jpg", 12, 10 ],
+	[ "../img/image1.jpg", 13, 10 ],
+	[ "../img/image1.jpg", 14, 10 ],
+	[ "../img/image1.jpg", 15, 10 ],
+	[ "../img/image1.jpg", 16, 10 ],
+	[ "../img/image1.jpg", 17, 10 ],
+	[ "../img/image1.jpg", 18, 10 ],
+	[ "../img/image1.jpg", 4, 7 ],
+	[ "../img/image1.jpg", 5, 7 ],
+	[ "../img/image1.jpg", 6, 7 ],
+	[ "../img/image1.jpg", 7, 7 ],
+	[ "../img/image1.jpg", 8, 7 ],
+	[ "../img/image1.jpg", 9, 7 ],
+	[ "../img/image1.jpg", 10, 7 ],
+	[ "../img/image1.jpg", 11, 7 ],
+	[ "../img/image1.jpg", 12, 7 ],
+	[ "../img/image1.jpg", 13, 7 ],
+	[ "../img/image1.jpg", 14, 7 ],
+	[ "../img/image1.jpg", 15, 7 ],
+	[ "../img/image1.jpg", 16, 7 ],
+	[ "../img/image1.jpg", 17, 7 ],
+	[ "../img/image1.jpg", 18, 7 ]
+];
+
+var camera, scene, renderer;
+var geometry, material, mesh;
+
+var controls;
+
+var objects = [];
+var targets = { sphere: [] };
+
+init();
+animate();
+
+function init() {
+
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 5000 );
+	camera.position.z = 1800;
+
+	scene = new THREE.Scene();
+
+	for ( var i = 0; i < table.length; i ++ ) {
+
+		var item = table[ i ];
+
+		var element = document.createElement( 'div' );
+		element.className = 'element';
+
+		var newImg = document.createElement( 'img' );
+		newImg.className = 'img';
+		newImg.src= item[ 0 ];
+		element.appendChild( newImg );
+
+
+		var object = new THREE.CSS3DObject( element );
+		object.position.x = Math.random() * 4000 - 2000;
+		object.position.y = Math.random() * 4000 - 2000;
+		object.position.z = Math.random() * 4000 - 2000;
+		scene.add( object );
+
+		objects.push( object );
+
+	}
+
+	// sphere
+
+	var vector = new THREE.Vector3();
+
+	for ( var i = 0, l = objects.length; i < l; i ++ ) {
+
+		var object = objects[ i ];
+
+		var phi = Math.acos( -1 + ( 2 * i ) / l );
+		var theta = Math.sqrt( l * Math.PI ) * phi;
+
+		var object = new THREE.Object3D();
+
+		object.position.x = 1000 * Math.cos( theta ) * Math.sin( phi );
+		object.position.y = 1000 * Math.sin( theta ) * Math.sin( phi );
+		object.position.z = 1000 * Math.cos( phi );
+
+		vector.copy( object.position ).multiplyScalar( 2 );
+
+		object.lookAt( vector );
+
+		targets.sphere.push( object );
+
+	}
+
+	renderer = new THREE.CSS3DRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.domElement.style.position = 'absolute';
+	renderer.domElement.style.top = 0;
+	document.getElementById('container').appendChild( renderer.domElement );
+
+
+	controls = new THREE.TrackballControls( camera, renderer.domElement );
+	controls.rotateSpeed = 1;
+	controls.addEventListener( 'change', render );
+
+    transform( targets.sphere, 0 );
+
+	window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function transform( targets, duration ) {
+	
+	TWEEN.removeAll();
+
+	for ( var i = 0; i < objects.length; i ++ ) {
+
+		var object = objects[ i ];
+		var target = targets[ i ];
+
+		new TWEEN.Tween( object.position )
+			.to( { x: target.position.x, y: target.position.y, z: target.position.z }, Math.random() * duration + duration )
+			.easing( TWEEN.Easing.Exponential.InOut )
+			.start();
+
+		new TWEEN.Tween( object.rotation )
+			.to( { x: target.rotation.x, y: target.rotation.y, z: target.rotation.z }, Math.random() * duration + duration )
+			.easing( TWEEN.Easing.Exponential.InOut )
+			.start();
+
+	}
+	new TWEEN.Tween( this )
+	.to( {}, duration * 2 )
+	.onUpdate( render )
+	.start();
+
+}
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function animate() {
+	requestAnimationFrame( animate );
+	TWEEN.update();
+	controls.update();
+}
+
+function render() {
+	renderer.render( scene, camera );
+}
